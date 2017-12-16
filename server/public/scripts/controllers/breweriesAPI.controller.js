c0972c6d0389c7c54d1c264d9fc6e0a989ffaf7e
Sarah Cooke <marshcooke1@gmail.com>
2017-12-16 14:52:14 -0600

    added breweriesAPI html and controller sourched in index

---
+++ server/public/scripts/controllers/breweriesAPI.controller.js
@@ -0,0 +1,18 @@
+myApp.controller('ApiController', function(UserService) {
+    console.log('ApiController created');
+    var vm = this;
+  
+    vm.allBreweriesObj = UserService.allBreweriesObj;  
+  
+    vm.getBreweries = function() {
+      UserService.getBreweries();
+          
+      console.log('brews: ', vm.allBreweriesObj);
+    }
+    
+    vm.addFavorites = function(brewery) {
+      UserService.addFavorites(brewery);
+  
+      console.log('add favorites hit in ic')
+    }
+  });
