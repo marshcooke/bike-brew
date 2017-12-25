4d0760fac35a9e9f5f782ffbd92a95f282ae907d
Sarah Cooke <marshcooke1@gmail.com>
2017-12-24 23:44:21 -0600

    controller formatting

--- server/public/scripts/controllers/favorites.controller.js
+++ server/public/scripts/controllers/favorites.controller.js
@@ -1,6 +1,7 @@
 myApp.controller('FavoritesController', function (UserService) {
   console.log('FavoritesController created');
   var vm = this;
+  
   vm.userService = UserService;
   vm.userObject = UserService.userObject;
 
--- server/public/scripts/controllers/login.controller.js
+++ server/public/scripts/controllers/login.controller.js
@@ -1,6 +1,7 @@
 myApp.controller('LoginController', function ($http, $location, UserService) {
   console.log('LoginController created');
   var vm = this;
+  
   vm.user = {
     username: '',
     password: ''
