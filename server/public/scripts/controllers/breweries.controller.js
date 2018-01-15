e2cb74aac7713819a4322cd95883e61d038facaf
Sarah Cooke <marshcooke1@gmail.com>
2018-01-15 14:45:03 -0600

    removing console logs from breweries controllers

--- server/public/scripts/controllers/breweries.controller.js
+++ server/public/scripts/controllers/breweries.controller.js
@@ -1,16 +1,16 @@
 myApp.controller('BreweriesController', function (UserService) {
-  console.log('BreweriesController created');
+  // console.log('BreweriesController created');
   var vm = this;
 
   vm.allBreweriesObj = UserService.allBreweriesObj;
 
   vm.getBreweries = function () {
     UserService.getBreweries();
-    console.log('brews: ', vm.allBreweriesObj);
+    // console.log('brews: ', vm.allBreweriesObj);
   }
 
   vm.addFavorites = function (brewery) {
     UserService.addFavorites(brewery);
-    console.log('add favorites hit in ic')
+    // console.log('add favorites hit in ic')
   }
 });
