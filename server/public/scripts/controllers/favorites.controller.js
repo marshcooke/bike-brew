d904326becf21e9388932123ba66c33074f53960
Sarah Cooke <marshcooke1@gmail.com>
2018-01-15 14:43:09 -0600

    removing favorites controller console log

--- server/public/scripts/controllers/favorites.controller.js
+++ server/public/scripts/controllers/favorites.controller.js
@@ -1,5 +1,5 @@
 myApp.controller('FavoritesController', function (UserService) {
-  console.log('FavoritesController created');
+  // console.log('FavoritesController created');
   var vm = this;
   
   vm.userService = UserService;
@@ -10,13 +10,13 @@ myApp.controller('FavoritesController', function (UserService) {
   vm.getFavorites = function () {
     UserService.getFavorites();
     // vm.allFavoritesObj = UserService.allFavoritesObj;
-    console.log('favorites: ', vm.allFavoritesObj);
+    // console.log('favorites: ', vm.allFavoritesObj);
   }
 
   vm.deleteFavorites = function (brewery) {
     UserService.deleteFavorites(brewery);
     // vm.allFavoritesObj = UserService.allFavoritesObj;
-    console.log('delete favorites hit in uc')
+    // console.log('delete favorites hit in uc')
     vm.getFavorites();
   }
 
