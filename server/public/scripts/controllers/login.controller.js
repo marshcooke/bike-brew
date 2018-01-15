bd6d6191d72c6e0bdb9511482caae68fd47883bf
Sarah Cooke <marshcooke1@gmail.com>
2018-01-15 14:39:29 -0600

    removing login controller console logs

--- server/public/scripts/controllers/login.controller.js
+++ server/public/scripts/controllers/login.controller.js
@@ -1,5 +1,5 @@
 myApp.controller('LoginController', function ($http, $location, UserService) {
-  console.log('LoginController created');
+  // console.log('LoginController created');
   var vm = this;
   
   vm.user = {
@@ -9,38 +9,38 @@ myApp.controller('LoginController', function ($http, $location, UserService) {
   vm.message = '';
 
   vm.login = function () {
-    console.log('LoginController -- login');
+    // console.log('LoginController -- login');
     if (vm.user.username === '' || vm.user.password === '') {
       vm.message = "Enter your username and password!";
     } else {
       // console.log('LoginController -- login -- sending to server...', vm.user);
       $http.post('/', vm.user).then(function (response) {
         if (response.data.username) {
-          console.log('LoginController -- login -- success: ', response.data);
+          // console.log('LoginController -- login -- success: ', response.data);
           // location works with SPA (ng-route)
           $location.path('/home'); // http://localhost:6172/#/user
         } else {
-          console.log('LoginController -- login -- failure: ', response);
+          // console.log('LoginController -- login -- failure: ', response);
           vm.message = "Try that again!";
         }
       }).catch(function (response) {
-        console.log('LoginController -- registerUser -- failure: ', response);
+        // console.log('LoginController -- registerUser -- failure: ', response);
         vm.message = "Try that again!";
       });
     }
   };
 
   vm.registerUser = function () {
-    console.log('LoginController -- registerUser');
+    // console.log('LoginController -- registerUser');
     if (vm.user.username === '' || vm.user.password === '') {
       vm.message = "Choose a username and password!";
     } else {
-      console.log('LoginController -- registerUser -- sending to server...', vm.user);
+      // console.log('LoginController -- registerUser -- sending to server...', vm.user);
       $http.post('/register', vm.user).then(function (response) {
-        console.log('LoginController -- registerUser -- success');
+        // console.log('LoginController -- registerUser -- success');
         $location.path('/login');
       }).catch(function (response) {
-        console.log('LoginController -- registerUser -- error');
+        // console.log('LoginController -- registerUser -- error');
         vm.message = "Please try again."
       });
     }
